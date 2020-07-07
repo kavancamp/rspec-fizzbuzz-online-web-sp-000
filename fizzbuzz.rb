0d7292325394d7db28b45251269b9b6e0ea2a859
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-07 21:34:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if i % 3 == 0 and i % 5 == 0
+  if int % 3 == 0 and int % 5 == 0
     "FizzBuzz"
 
    elsif int % 3 == 0
